61db7262bee2cb5c191498d6bc76ff10d42b9744
Chase Davis <chase.davis@gmail.com>
2012-06-26 14:31:35 -0700

    No more geos

--- rainmaker/settings/production.py
+++ rainmaker/settings/production.py
@@ -26,8 +26,8 @@ STATIC_URL = 'http://media.apps.cironline.org/rainmaker-wa/site_media/'
 ADMIN_MEDIA_PREFIX = 'http://media.apps.cironline.org/rainmaker-wa/site_media/admin/'
 
 # GEOS paths for GeoDjango and GDAL. Configured for our particular Heroku setup.
-GEOS_LIBRARY_PATH = '/app/.geodjango/geos/lib/libgeos_c.so'
-GDAL_LIBRARY_PATH = '/app/.geodjango/gdal/lib/libgdal.so'
+#GEOS_LIBRARY_PATH = '/app/.geodjango/geos/lib/libgeos_c.so'
+#GDAL_LIBRARY_PATH = '/app/.geodjango/gdal/lib/libgdal.so'
 
 # Caching
 CACHE_MIDDLEWARE_SECONDS = 90 * 60 # 90 minutes
